032d4a141ad6e8aa25f7c380228c5df1bb9658cf
Sergio <chicoboliviano16@gmail.com>
2020-09-08 22:31:00 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello0o thereee</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAX76ls84AuwW6W4zGBx7JkLytThDiqh2c",
+  authDomain: "clone-c577d.firebaseapp.com",
+  databaseURL: "https://clone-c577d.firebaseio.com",
+  projectId: "clone-c577d",
+  storageBucket: "clone-c577d.appspot.com",
+  messagingSenderId: "854730721700",
+  appId: "1:854730721700:web:b5cfc81ade4d0eff7b9931",
+  measurementId: "G-PHYSV0SP2W",
+};
